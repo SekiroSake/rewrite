4c575ba0e1f2f343f189f7dc32abb1999677056a
Hang Wang <hangwan@microsoft.com>
2019-11-11 16:58:36 -0800

    current stable version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +39,7 @@ import { RfqComponent } from './body/dashboard/rfq/rfq.component';
       BrowserModule,
       AppRoutingModule,
       FormsModule,
+      ReactiveFormsModule,
       NgbModule
    ],
    providers: [],
--- src/app/body/new_matter_proposal/new_matter_proposal.component.ts
+++ src/app/body/new_matter_proposal/new_matter_proposal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 import {Matter} from '../../Model/Matter';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new_matter_proposal',
@@ -8,11 +9,10 @@ import {Matter} from '../../Model/Matter';
   styleUrls: ['./new_matter_proposal.component.css']
 })
 export class New_matter_proposalComponent implements OnInit {
+  matterForm: FormGroup;
   matterModel = new Matter(1,"","","","","","",false);
-  model: NgbDateStruct;
   startDatemodel: NgbDateStruct;
   endDatemodel: NgbDateStruct;
-  date: {year: number, month: number, day: number};
   startDate: {year: number, month: number, day: number};
   endDate: {year: number, month: number, day: number};
   firmCountries = ['', 'United State',
@@ -21,15 +21,30 @@ export class New_matter_proposalComponent implements OnInit {
   'Tier 1', 'Tier 2', 'Tier 3'];
   submitted = false;
   onSubmit() {this.submitted = true; alert(JSON.stringify(this.matterModel));}
-  get diagnostic() { return JSON.stringify(this.matterModel); }
+  get diagnostic() { return JSON.stringify(this.matterForm.value); }
 
   constructor(private calendar: NgbCalendar) { }
 
   ngOnInit() {
+    this.matterForm = new FormGroup({
+      title: new FormControl(),
+      description: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      lfeStartDate: new FormControl(null),
+      lfeEndDate: new FormControl(null),
+      firmCountry: new FormControl(),
+      matterTier: new FormControl(),
+      exceed100k: new FormControl('no')
+    }, this.dateValidator);
   }
 
-  selectToday() {
-    this.model = this.calendar.getToday();
+  dateValidator(g: FormGroup){
+    //g.get('lfeEndDate').value > g.get('lfeStartDate').value
+    // const start = new Date(g.get('lfeEndDate').value);
+    // const end = new Date(g.get('lfeEndDate').value);
+    return  2 > 1 ? null : {'mismatch': true};
   }
 
+  createMatter() {
+    console.log(this.matterForm.value);
+  }
 }
